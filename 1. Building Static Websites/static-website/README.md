f0bb786406d035b9359fda5ad235de42fb8cfc92
Vakho <vakho10@gmail.com>
2025-02-25 20:28:31 +0400

    Add first lesson example

---
+++ 1. Building Static Websites/static-website/README.md
@@ -0,0 +1,6 @@
+# Static Website
+
+ეს პროექტი წარმოადგენს სტატიკური ვებსაიტის მაგალითს, დაწერილს Spring Boot-ზე.
+
+## External Dependencies
+* https://ultraq.github.io/thymeleaf-layout-dialect/
---
+++ 1. Building Static Websites/static-website/src/test/java/com/example/static_website/StaticWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.static_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
